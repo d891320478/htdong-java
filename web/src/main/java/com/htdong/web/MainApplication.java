8b922fc52e284b29bd12d8446b1ee5c7573dd9f8
usr <dht31261@gmail.com>
2021-11-09 20:13:01 +0800

    c

--- web/src/main/java/com/htdong/web/MainApplication.java
+++ web/src/main/java/com/htdong/web/MainApplication.java
@@ -1,8 +1,21 @@
 package com.htdong.web;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+import lombok.extern.slf4j.Slf4j;
+
+@SpringBootApplication(scanBasePackages = { "com.shinemo.htdong" })
+@EnableScheduling
+@Slf4j
 public class MainApplication {
 
     public static void main(String[] args) {
-        
+        try {
+            SpringApplication.run(MainApplication.class, args);
+        } catch (Throwable e) {
+            log.error("main.", e);
+        }
     }
 }
---
+++ web/src/main/java/com/htdong/web/controller/SysController.java
@@ -0,0 +1,13 @@
+package com.htdong.web.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SysController {
+
+    @GetMapping("/checkstatus")
+    public String checkstatus() {
+        return "success";
+    }
+}
