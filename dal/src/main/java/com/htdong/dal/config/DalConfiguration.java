885e3e33c020fd30be8e25d420f7e9fad732b089
usr <dht31261@gmail.com>
2021-11-11 16:35:43 +0800

    c

--- dal/src/main/java/com/htdong/dal/config/DalConfiguration.java
+++ dal/src/main/java/com/htdong/dal/config/DalConfiguration.java
@@ -19,35 +19,35 @@ import com.alibaba.druid.pool.DruidDataSource;
 @MapperScan(basePackages = { "com.htdong.dal.mapper" })
 public class DalConfiguration {
 
-//    @Bean(initMethod = "init")
-//    public DruidDataSource dataSource(@Value("${jdbc.url}") String url, @Value("${jdbc.user}") String user,
-//            @Value("${jdbc.password}") String password) {
-//        DruidDataSource dataSource = new DruidDataSource();
-//        dataSource.setUrl(url);
-//        dataSource.setUsername(user);
-//        dataSource.setPassword(password);
-//        dataSource.setInitialSize(5);
-//        dataSource.setMinIdle(5);
-//        dataSource.setMaxActive(50);
-//        dataSource.setMaxWait(60000);
-//        dataSource.setTimeBetweenEvictionRunsMillis(60000);
-//        dataSource.setMinEvictableIdleTimeMillis(300000);
-//        dataSource.setValidationQuery("SELECT 'x'");
-//        dataSource.setTestWhileIdle(true);
-//        dataSource.setTestOnBorrow(false);
-//        dataSource.setTestOnReturn(false);
-//        dataSource.setPoolPreparedStatements(true);
-//        dataSource.setMaxPoolPreparedStatementPerConnectionSize(20);
-//        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
-//        return dataSource;
-//    }
-//
-//    @Bean
-//    public SqlSessionFactoryBean sqlSessionFactoryBean(
-//            @Autowired @Qualifier(value = "dataSource") DataSource dataSource) throws IOException {
-//        SqlSessionFactoryBean bean = new SqlSessionFactoryBean();
-//        bean.setDataSource(dataSource);
-//        bean.setMapperLocations(new PathMatchingResourcePatternResolver().getResources("classpath*:sqlmap/*.xml"));
-//        return bean;
-//    }
+    @Bean(initMethod = "init")
+    public DruidDataSource dataSource(@Value("${jdbc.url}") String url, @Value("${jdbc.user}") String user,
+            @Value("${jdbc.password}") String password) {
+        DruidDataSource dataSource = new DruidDataSource();
+        dataSource.setUrl(url);
+        dataSource.setUsername(user);
+        dataSource.setPassword(password);
+        dataSource.setInitialSize(5);
+        dataSource.setMinIdle(5);
+        dataSource.setMaxActive(50);
+        dataSource.setMaxWait(60000);
+        dataSource.setTimeBetweenEvictionRunsMillis(60000);
+        dataSource.setMinEvictableIdleTimeMillis(300000);
+        dataSource.setValidationQuery("SELECT 'x'");
+        dataSource.setTestWhileIdle(true);
+        dataSource.setTestOnBorrow(false);
+        dataSource.setTestOnReturn(false);
+        dataSource.setPoolPreparedStatements(true);
+        dataSource.setMaxPoolPreparedStatementPerConnectionSize(20);
+        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+        return dataSource;
+    }
+
+    @Bean
+    public SqlSessionFactoryBean sqlSessionFactoryBean(
+            @Autowired @Qualifier(value = "dataSource") DataSource dataSource) throws IOException {
+        SqlSessionFactoryBean bean = new SqlSessionFactoryBean();
+        bean.setDataSource(dataSource);
+        bean.setMapperLocations(new PathMatchingResourcePatternResolver().getResources("classpath*:sqlmap/*.xml"));
+        return bean;
+    }
 }
