ff985f217837c80dee03a0c6304b661192479c20
htdong <dht31261@gmail.com>
2022-02-25 17:59:20 +0800

    c

--- web/src/main/java/com/htdong/web/MainApplication.java
+++ web/src/main/java/com/htdong/web/MainApplication.java
@@ -2,12 +2,11 @@ package com.htdong.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 import lombok.extern.slf4j.Slf4j;
 
-@PropertySource(value = {"file:/root/conf/application.conf"})
+//@PropertySource(value = {"file:/root/conf/application.conf"})
 @SpringBootApplication(scanBasePackages = { "com.htdong" })
 @EnableScheduling
 @Slf4j
