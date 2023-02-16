d39d8e869f342799ba3170a917ca554c3faa4dcf
htdong <dht31261@gmail.com>
2023-02-16 20:29:40 +0800

    c

--- core/src/main/java/com/htdong/core/bilibili/service/impl/BiliServiceImpl.java
+++ core/src/main/java/com/htdong/core/bilibili/service/impl/BiliServiceImpl.java
@@ -5,6 +5,9 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
+import org.apache.dubbo.config.annotation.DubboService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
@@ -14,14 +17,13 @@ import com.htdong.client.domain.result.ApiResult;
 import com.htdong.common.util.JacksonUtil;
 import com.htdong.core.bilibili.service.BiliService;
 
-import lombok.extern.slf4j.Slf4j;
-
-@Slf4j
+@DubboService
 @Service("biliService")
 public class BiliServiceImpl implements BiliService {
 
-    private static final TypeReference<ApiResult<RoomInitDTO>> ROOM_INIT_TYPE = new TypeReference<>() {
-    };
+    private static final Logger httplog = LoggerFactory.getLogger("HTTP_LOG");
+
+    private static final TypeReference<ApiResult<RoomInitDTO>> ROOM_INIT_TYPE = new TypeReference<>() {};
 
     private static final String BILIBILI_API = "https://api.live.bilibili.com";
     private static final String BILIBILI_LIVE_ROOM_INIT = "/room/v1/Room/room_init?id=%s";
@@ -45,13 +47,13 @@ public class BiliServiceImpl implements BiliService {
         HttpClient client = HttpClient.newHttpClient();
         try {
             HttpResponse<String> response = client.send(req, HttpResponse.BodyHandlers.ofString());
-            log.info("url = {}, code = {}, res = {}.", url, response.statusCode(), response.body());
+            httplog.info("url = {}, code = {}, res = {}.", url, response.statusCode(), response.body());
             if (response.statusCode() == HttpStatus.OK.value()) {
                 return JacksonUtil.getObjectMapper().readValue(response.body(), type);
             }
             return ApiResult.fail(response.statusCode(), "bilibili api http error.");
         } catch (Throwable e) {
-            log.error("url = {}", url, e);
+            httplog.error("url = {}", url, e);
             return ApiResult.fail(502, "exception");
         }
     }
