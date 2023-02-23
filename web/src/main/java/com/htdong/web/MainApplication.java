ffd7172357025e135729808570378ccf8a9e7648
htdong <dht31261@gmail.com>
2023-02-23 19:53:43 +0800

    c

--- web/src/main/java/com/htdong/web/MainApplication.java
+++ web/src/main/java/com/htdong/web/MainApplication.java
@@ -5,8 +5,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-import com.htdong.common.util.LocalConfFileInitUtil;
-
 import lombok.extern.slf4j.Slf4j;
 
 @SpringBootApplication(scanBasePackages = {"com.htdong"})
@@ -17,7 +15,6 @@ public class MainApplication {
 
     public static void main(String[] args) {
         try {
-            LocalConfFileInitUtil.init(System.getProperty("local.conf.file"));
             SpringApplication.run(MainApplication.class, args);
         } catch (Throwable e) {
             log.error("main.", e);
