c570486a6899aa8b6ffea602a73acd12d68da8e8
htdong <dht31261@gmail.com>
2023-02-24 16:38:05 +0800

    c

--- dal/src/main/java/com/htdong/dal/mapper/ShortUrlMapper.java
+++ dal/src/main/java/com/htdong/dal/mapper/ShortUrlMapper.java
@@ -3,6 +3,4 @@ package com.htdong.dal.mapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.htdong.client.domain.db.ShortUrlDO;
 
-public interface ShortUrlMapper extends BaseMapper<ShortUrlDO> {
-
-}
+public interface ShortUrlMapper extends BaseMapper<ShortUrlDO> {}
