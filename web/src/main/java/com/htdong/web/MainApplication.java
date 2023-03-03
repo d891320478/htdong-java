c211e77ad34e8f42085bb79eae8b39ce5524408a
htdong <dht31261@gmail.com>
2023-03-03 12:23:00 +0800

    c

--- web/src/main/java/com/htdong/web/MainApplication.java
+++ web/src/main/java/com/htdong/web/MainApplication.java
@@ -1,9 +1,13 @@
 package com.htdong.web;
 
+import java.util.Map;
+
 import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import com.htdong.common.util.LocalConfigUtil;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -14,6 +18,10 @@ import lombok.extern.slf4j.Slf4j;
 public class MainApplication {
 
     public static void main(String[] args) {
+        Map<String, String> map = LocalConfigUtil.getMap(System.getProperty("local.conf.file"));
+        for (Map.Entry<String, String> iter : map.entrySet()) {
+            System.setProperty(iter.getKey(), iter.getValue());
+        }
         try {
             SpringApplication.run(MainApplication.class, args);
         } catch (Throwable e) {
