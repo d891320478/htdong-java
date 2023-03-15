8a7e80ceb3b73fa12942890db12d8bfede1ef810
htdong <dht31261@gmail.com>
2023-03-15 22:38:57 +0800

    c

--- core/src/main/java/com/htdong/core/config/CoreConfig.java
+++ core/src/main/java/com/htdong/core/config/CoreConfig.java
@@ -4,11 +4,7 @@ import java.time.LocalDateTime;
 
 import javax.annotation.PreDestroy;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import com.htdong.client.conf.ConfigRegister;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -16,11 +12,11 @@ import lombok.extern.slf4j.Slf4j;
 @Configuration
 public class CoreConfig {
 
-    @Bean(initMethod = "init")
-    public ConfigRegister configRegister(@Value("${local.conf.file}") String configFilePath) {
-        ConfigRegister configRegister = new ConfigRegister(configFilePath);
-        return configRegister;
-    }
+    // @Bean(initMethod = "init")
+    // public ConfigRegister configRegister(@Value("${local.conf.file}") String configFilePath) {
+    // ConfigRegister configRegister = new ConfigRegister(configFilePath);
+    // return configRegister;
+    // }
 
     @PreDestroy
     public void preDestroy() {
