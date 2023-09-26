1b02e31ba4e11538782e29211572720d0fc9177f
htdong <dht31261@gmail.com>
2023-09-26 15:31:08 +0800

    c

---
+++ common/src/main/java/com/htdong/common/util/SignUtil.java
@@ -0,0 +1,31 @@
+package com.htdong.common.util;
+
+import java.security.MessageDigest;
+
+import org.apache.commons.codec.binary.Hex;
+import org.apache.commons.codec.digest.MessageDigestAlgorithms;
+
+public class SignUtil {
+
+    public static String md5Str(byte[] origin) {
+        try {
+            MessageDigest messagedigest = MessageDigest.getInstance(MessageDigestAlgorithms.MD5);
+            messagedigest.update(origin);
+            return Hex.encodeHexString(messagedigest.digest(origin));
+        } catch (Throwable e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static String sha256Str(byte[] origin) {
+        try {
+            MessageDigest messagedigest = MessageDigest.getInstance(MessageDigestAlgorithms.SHA_256);
+            messagedigest.update(origin);
+            return Hex.encodeHexString(messagedigest.digest(origin));
+        } catch (Throwable e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
