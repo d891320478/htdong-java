1fe7de2184938a0c4ca4ed4cca3130cc6f2e1fd1
htdong <dht31261@gmail.com>
2023-11-17 20:13:17 +0800

    c

--- common/src/main/java/com/htdong/common/rpc/bili/BiliRpcServiceDubbo.java
+++ common/src/main/java/com/htdong/common/rpc/bili/BiliRpcServiceDubbo.java
@@ -1,24 +1,21 @@
-    package com.htdong.common.rpc.bili;
+package com.htdong.common.rpc.bili;
 
-import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.atomic.AtomicBoolean;
 
-@javax.annotation.Generated(
-value = "by Dubbo generator",
-comments = "Source: bili.proto")
+@jakarta.annotation.Generated(value = "by Dubbo generator", comments = "Source: bili.proto")
 public final class BiliRpcServiceDubbo {
-private static final AtomicBoolean registered = new AtomicBoolean();
+    private static final AtomicBoolean registered = new AtomicBoolean();
 
-public static boolean init() {
-    if (registered.compareAndSet(false, true)) {
-            org.apache.dubbo.common.serialize.protobuf.support.ProtobufUtils.marshaller(
-            com.google.protobuf.Int64Value.getDefaultInstance());
-            org.apache.dubbo.common.serialize.protobuf.support.ProtobufUtils.marshaller(
-            com.google.protobuf.BoolValue.getDefaultInstance());
+    public static boolean init() {
+        if (registered.compareAndSet(false, true)) {
+            org.apache.dubbo.common.serialize.protobuf.support.ProtobufUtils
+                .marshaller(com.google.protobuf.Int64Value.getDefaultInstance());
+            org.apache.dubbo.common.serialize.protobuf.support.ProtobufUtils
+                .marshaller(com.google.protobuf.BoolValue.getDefaultInstance());
+        }
+        return true;
     }
-    return true;
-}
 
-private BiliRpcServiceDubbo() {}
+    private BiliRpcServiceDubbo() {}
 
 }
