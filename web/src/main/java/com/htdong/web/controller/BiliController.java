f0d2c8c018be13d4e5959a4a19ab2ae7c31b7a18
htdong <dht31261@gmail.com>
2024-08-05 14:58:43 +0800

    礼物列表

--- web/src/main/java/com/htdong/web/controller/BiliController.java
+++ web/src/main/java/com/htdong/web/controller/BiliController.java
@@ -1,10 +1,16 @@
 package com.htdong.web.controller;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileWriter;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Base64;
+import java.util.Scanner;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,6 +28,29 @@ import lombok.extern.slf4j.Slf4j;
 @RequestMapping("/bili")
 @Slf4j
 public class BiliController {
+
+    @GetMapping("/giftList")
+    public ResponseEntity<String> giftList() throws FileNotFoundException {
+        LocalDate today = LocalDate.now();
+        DateTimeFormatter format = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        StringBuilder sb = new StringBuilder();
+        File f = new File("/data/biliDanMu222272/" + today.format(format) + ".log");
+        if (f.exists()) {
+            try (Scanner in = new Scanner(f)) {
+                while (in.hasNextLine()) {
+                    String s = in.nextLine();
+                    if (s.substring(20).startsWith("[SC]")) {
+                        sb.append(s).append("<br/>");
+                    } else if (s.substring(20).startsWith("[Gift]")) {
+                        sb.append(s).append("<br/>");
+                    } else if (s.substring(20).startsWith("[Guard]")) {
+                        sb.append(s).append("<br/>");
+                    }
+                }
+            }
+        }
+        return ResponseEntity.ok(sb.toString());
+    }
 
     @PostMapping("/updateCookie")
     public ResponseEntity<Void> updateCookie(@RequestBody String cookieStr) {
