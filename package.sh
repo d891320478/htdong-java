a6a1cb330a0baf1ebcc4750e5e672c21887db28e
htdong <dht31261@gmail.com>
2025-09-18 17:04:19 +0800

    c

--- package.sh
+++ package.sh
@@ -4,6 +4,10 @@ set -e
 
 mvn clean package -Dprotoc.skip=true -Dmaven.test.skip=true -U
 cd web/target
-zip htdong.zip htdong.jar
-curl -F 'file=@htdong.zip' -H "uploadTmpToken: ${UPLOAD_TMP_TOKEN}" http://${ALIYUN_HOST}:9961/htdong/upload/tmp
+if [[ -f ~/script/aliyun/207-scp-aliyun.sh ]];then
+  sh ~/script/aliyun/207-scp-aliyun.sh htdong.jar
+else
+  zip htdong.zip htdong.jar
+  curl -F 'file=@htdong.zip' -H "uploadTmpToken: ${UPLOAD_TMP_TOKEN}" http://${ALIYUN_HOST}:9961/htdong/upload/tmp
+fi
 
