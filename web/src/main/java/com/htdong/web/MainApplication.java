f7f40b26db56310781130428a23aab77ed22cf07
htdong <dht31261@gmail.com>
2025-09-19 10:05:36 +0800

    注册http端口到zk

--- web/src/main/java/com/htdong/web/MainApplication.java
+++ web/src/main/java/com/htdong/web/MainApplication.java
@@ -3,6 +3,7 @@ package com.htdong.web;
 import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 import lombok.extern.slf4j.Slf4j;
@@ -11,6 +12,7 @@ import lombok.extern.slf4j.Slf4j;
 @EnableScheduling
 @EnableDubbo(scanBasePackages = {"com.htdong"}, multipleConfig = true)
 @Slf4j
+@EnableDiscoveryClient
 public class MainApplication {
 
     public static void main(String[] args) {
